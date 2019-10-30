54289bf2678f8cd3a30acdbea68ef709838ef394
Murrx <altenarobin@gmail.com>
2019-10-30 19:27:33 +0000

    initial step in getting google auth to work

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 source "https://rubygems.org"
-gem 'sinatra-google-auth'
+gem 'sinatra'
+gem 'json'
+gem 'oauth2'
 
 gemspec
--- lib/jekyll_auth/auth_site.rb
+++ lib/jekyll_auth/auth_site.rb
@@ -1,7 +1,28 @@
 # frozen_string_literal: true
 require 'sinatra'
-require 'sinatra/google-auth'
-require 'mail'
+require 'oauth2'
+require 'json'
+
+enable :sessions
+
+SCOPES = ['https://www.googleapis.com/auth/userinfo.email'].join(' ')
+
+unless G_API_CLIENT = ENV['G_API_CLIENT']
+  raise "You must specify the G_API_CLIENT env variable"
+end
+
+unless G_API_SECRET = ENV['G_API_SECRET']
+  raise "You must specify the G_API_SECRET env variable"
+end
+
+def client
+  client ||= OAuth2::Client.new(G_API_CLIENT, G_API_SECRET, {
+    :site => 'https://accounts.google.com',
+    :authorize_url => '/o/oauth2/auth',
+    :token_url => '/o/oauth2/token'
+  })
+end
+
 
 class JekyllAuth
   class AuthSite < Sinatra::Base
@@ -13,7 +34,6 @@ class JekyllAuth
 #    use Rack::Session::Cookie,       :http_only => true,
 #                                     :secret    => ENV["SESSION_SECRET"] || SecureRandom.hex
 
-    register Sinatra::GoogleAuth
 
     use Rack::Logger
 
@@ -22,9 +42,33 @@ class JekyllAuth
 #      pass if whitelisted?
 #      authenticate
 #    end
-    get '*' do
-      authenticate
-      'hello'
+    get '/' do
+      erb :index
+#      authenticate
+#      'hello'
+    end
+
+    get "/auth" do
+     redirect client.auth_code.authorize_url(:redirect_uri => redirect_uri,:scope => SCOPES,:access_type => "offline")
+    end
+
+    get '/oauth2callback' do
+      access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
+      session[:access_token] = access_token.token
+      @message = "Successfully authenticated with the server"
+      @access_token = session[:access_token]
+
+      # parsed is a handy method on an OAuth2::Response object that will
+      # intelligently try and parse the response.body
+      @email = access_token.get('https://www.googleapis.com/userinfo/email?alt=json').parsed
+      erb :success
+    end
+
+    def redirect_uri
+      uri = URI.parse(request.url)
+      uri.path = '/oauth2callback'
+      uri.query = nil
+      uri.to_s
     end
 
     get "/logout" do
