109737ce218663c4d9d4f5037eaad1646dbe0a8f
Murrx <altenarobin@gmail.com>
2019-11-03 13:43:13 +0000

    inial implementation of email whitelist based authentication

--- lib/jekyll_auth/auth_site.rb
+++ lib/jekyll_auth/auth_site.rb
@@ -6,6 +6,8 @@ require 'json'
 enable :sessions
 
 SCOPES = ['https://www.googleapis.com/auth/userinfo.email'].join(' ')
+EMAIL_FILE = File.read('whitelisted_email.json')
+WHITELISTED_EMAILS = JSON.parse(EMAIL_FILE)["emails"]
 
 unless G_API_CLIENT = ENV['G_API_CLIENT']
   raise "You must specify the G_API_CLIENT env variable"
@@ -23,34 +25,52 @@ def client
   })
 end
 
-
 class JekyllAuth
   class AuthSite < Sinatra::Base
-#    configure :production do
-#      require "rack-ssl-enforcer"
-#      use Rack::SslEnforcer if JekyllAuth.ssl?
-#    end
+    configure :production, :development do
+      enable :logging
+    end
+    configure :production do
+      require "rack-ssl-enforcer"
+      use Rack::SslEnforcer if JekyllAuth.ssl?
+    end
 
-#    use Rack::Session::Cookie,       :http_only => true,
-#                                     :secret    => ENV["SESSION_SECRET"] || SecureRandom.hex
-
-
-    use Rack::Logger
+    use Rack::Session::Cookie,       :http_only => true,
+                                     :secret    => ENV["SESSION_SECRET"] || SecureRandom.hex
 
     get '/' do
-     redirect client.auth_code.authorize_url(:redirect_uri => redirect_uri,:scope => SCOPES,:access_type => "offline")
+      authenticate
+      'welcome'
+#      logger.info(session["email"])
+    end
+
+    def authenticate
+      unless session["user"]
+        session['google-auth-redirect'] = request.path
+        redirect client.auth_code.authorize_url(:redirect_uri => redirect_uri,:scope => SCOPES,:access_type => "offline")
+      end
     end
 
     get '/oauth2callback' do
-      access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
-      session[:access_token] = access_token.token
-      @message = "Successfully authenticated with the server"
-      @access_token = session[:access_token]
+      unless session["user"]
+        access_token = client.auth_code.get_token(params[:code], :redirect_uri => redirect_uri)
+        session[:access_token] = access_token.token
+        @message = "Successfully authenticated with the server"
+        @access_token = session[:access_token]
+        session["user"] = access_token.get('https://www.googleapis.com/oauth2/v2/userinfo').parsed
+	session["email"] = session["user"]["email"]
+	session["isWhitelisted"] = isWhitelisted(session["email"])
+      end
+      if session["isWhitelisted"]
+        url = session['google-auth-redirect'] || to("/")
+        redirect url
+      else
+        'forbidden'
+      end
+    end
 
-      # parsed is a handy method on an OAuth2::Response object that will
-      # intelligently try and parse the response.body
-      @email = access_token.get('https://www.googleapis.com/oauth2/v2/userinfo').parsed
-      erb :success
+    def isWhitelisted(email)
+	    return WHITELISTED_EMAILS.include? email 
     end
 
     def redirect_uri
@@ -66,3 +86,4 @@ class JekyllAuth
     end
   end
 end
+
