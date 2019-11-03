6405d8769fb90d92ac978442ceec4083e2c807a3
Murrx <altenarobin@gmail.com>
2019-11-03 17:08:57 +0000

    move to helpers

--- lib/jekyll_auth/auth_site.rb
+++ lib/jekyll_auth/auth_site.rb
@@ -6,8 +6,6 @@ require 'json'
 enable :sessions
 
 SCOPES = ['https://www.googleapis.com/auth/userinfo.email'].join(' ')
-EMAIL_FILE = File.read('whitelisted_email.json')
-WHITELISTED_EMAILS = JSON.parse(EMAIL_FILE)["emails"]
 
 unless G_API_CLIENT = ENV['G_API_CLIENT']
   raise "You must specify the G_API_CLIENT env variable"
@@ -15,14 +13,6 @@ end
 
 unless G_API_SECRET = ENV['G_API_SECRET']
   raise "You must specify the G_API_SECRET env variable"
-end
-
-def client
-  client ||= OAuth2::Client.new(G_API_CLIENT, G_API_SECRET, {
-    :site => 'https://accounts.google.com',
-    :authorize_url => '/o/oauth2/auth',
-    :token_url => '/o/oauth2/token'
-  })
 end
 
 class JekyllAuth
@@ -70,10 +60,6 @@ class JekyllAuth
         redirect to("/login.html")
       end
       pass
-    end
-
-    def isWhitelisted(email)
-	    return WHITELISTED_EMAILS.include? email 
     end
 
     def redirect_uri
--- lib/jekyll_auth/helpers.rb
+++ lib/jekyll_auth/helpers.rb
@@ -1,10 +1,25 @@
 # frozen_string_literal: true
+require 'json'
 
+EMAIL_FILE = File.read('whitelisted_email.json')
+WHITELISTED_EMAILS = JSON.parse(EMAIL_FILE)["emails"]
 class JekyllAuth
   module Helpers
+
+    def client
+      client ||= OAuth2::Client.new(G_API_CLIENT, G_API_SECRET, {
+        :site => 'https://accounts.google.com',
+        :authorize_url => '/o/oauth2/auth',
+        :token_url => '/o/oauth2/token'
+      })
+end
+    def isWhitelisted(email)
+	    return WHITELISTED_EMAILS.include? email 
+    end
     def whitelisted?
       return true if request.path_info == "/logout"
       return true if request.path_info == "/login.html"
+      return true if request.path_info == "/google-auth"
       !!(JekyllAuth.whitelist && JekyllAuth.whitelist.match(request.path_info))
     end
   end
